817c712ea7964505cf1b70b87442176e30ae0d2c
Sam Peterson <speterson@reachmedianetwork.com>
2023-11-30 23:21:15 +0100

    Add: part 2 of day 1.

--- src/2023/day1.ts
+++ src/2023/day1.ts
@@ -22,7 +22,81 @@ export function day1Part1(values: string[]) {
   return { decodedNumbers: decodedNumbers, sum: sum };
 }
 
-export const day1Part1ExampleData = [
+const VALID_NUMBER_STRINGS = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+] as const;
+
+function convertValidNumbersToValue(numString: string) {
+  const expectedType = numString as (typeof VALID_NUMBER_STRINGS)[number];
+  switch (expectedType) {
+    case "eight":
+      return 8;
+    case "five":
+      return 5;
+    case "four":
+      return 4;
+    case "nine":
+      return 9;
+    case "one":
+      return 1;
+    case "seven":
+      return 7;
+    case "six":
+      return 6;
+    case "three":
+      return 3;
+    case "two":
+      return 2;
+    default:
+      const invalidValue: (typeof VALID_NUMBER_STRINGS)[number] = expectedType;
+      throw new Error("Invalid value passed", { cause: invalidValue });
+  }
+}
+
+export function day1Part2(values: string[]) {
+  let sum = 0;
+  const decodedNumbers: number[] = [];
+  const validStrings = VALID_NUMBER_STRINGS.join("|");
+  const firstRegexObj = new RegExp(`${validStrings}|[0-9]`);
+  const lastRegexObj = new RegExp(`.*(${validStrings}|[0-9])`);
+
+  values.forEach((value) => {
+    const firstMatch = value.toLowerCase().match(firstRegexObj)?.at(0);
+    let lastMatch = value.toLowerCase().match(lastRegexObj)?.at(1);
+
+    if (firstMatch === undefined || lastMatch === undefined) {
+      console.error(value);
+      throw new Error("Invalid values", {
+        cause: `first: ${firstMatch} - last: ${lastMatch}`,
+      });
+    }
+
+    let convertedFirstNumber = Number(firstMatch);
+    let convertedLastNumber = Number(lastMatch);
+    if (isNaN(convertedFirstNumber))
+      convertedFirstNumber = convertValidNumbersToValue(firstMatch);
+    if (isNaN(convertedLastNumber))
+      convertedLastNumber = convertValidNumbersToValue(lastMatch);
+
+    const combinedNumber = Number(
+      `${convertedFirstNumber}${convertedLastNumber}`
+    );
+    sum += combinedNumber;
+    decodedNumbers.push(combinedNumber);
+  });
+
+  return { decodedNumbers: decodedNumbers, sum: sum };
+}
+
+export const day1ExampleData = [
   "ckmb52fldxkseven3fkjgcbzmnr7",
   "gckhqpb6twoqnjxqplthree2fourkspnsnzxlz1",
   "2onetwocrgbqm7",
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('I do nothing! -- currently');
+import { day1ExampleData, day1Part1, day1Part2 } from "./2023";
+
+export * from "./2023";
+
+// Run tests here?
+
+console.log(day1Part1(day1ExampleData).sum);
+
+console.log(day1Part2(day1ExampleData).sum);
