c55f72854845a9fbee77ffec6ff12dca2fe3426a
Sam Peterson <speterson@reachmedianetwork.com>
2023-11-30 22:14:43 +0100

    INIT: Day1

---
+++ src/2023/day1.ts
@@ -0,0 +1,13 @@
+export function day1Part1(values: string[]) {
+  let sum = 0;
+  const decodedNumbers: number[] = [];
+
+  return { decodedNumbers: decodedNumbers, sum: sum };
+}
+
+export const day1Part1ExampleData = [
+  "1abc2",
+  "pqr3stu8vwx",
+  "a1b2c3d4e5f",
+  "treb7uchet",
+];
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { day1Part1, day1Part1ExampleData } from "./2023";
+
+export * from "./2023";
+
+// Run tests here?
+
+console.log(day1Part1(day1Part1ExampleData));
