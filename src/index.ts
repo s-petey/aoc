061143de0021b314a2c3cb3ca0e68e988da158be
Sam Peterson <samllama55@gmail.com>
2023-12-02 09:29:16 +0100

    CHANGE: To bun, add test instead of index.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import { day1ExampleData, day1Part1, day1Part2 } from "./2023";
-
-export * from "./2023";
-
-// Run tests here?
-
-console.log(day1Part1(day1ExampleData).sum);
-
-console.log(day1Part2(day1ExampleData).sum);
+export * from './2023';
